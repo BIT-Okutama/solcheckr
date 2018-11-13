248aa6b3bfa6e7da3782339a35fa114ffd6e33fc
Sam Benemerito <benemeritosam@gmail.com>
2018-11-13 15:17:38 +0900

    initial Audit model

--- checkr/models.py
+++ checkr/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = ((1, 'Finished'), (2, 'Error'), (3, 'Expired'),
+                  (4, 'Performing'), (5, 'Queued'))
+
+
+class Audit(models.Model):
+    email = models.EmailField()
+    contract = models.TextField()
+    report = models.TextField(blank=True, default='')
+    submitted = models.DateTimeField(auto_now=True)
+    status = models.PositiveIntegerField(default=5, choices=STATUS_CHOICES)
