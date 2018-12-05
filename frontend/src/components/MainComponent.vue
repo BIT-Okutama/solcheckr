7c9137a51f48cc73707b53c2306fa0d99ff36cb4
Sam Benemerito <benemeritosam@gmail.com>
2018-12-05 16:01:09 +0900

    fix interface of first page

--- frontend/src/components/MainComponent.vue
+++ frontend/src/components/MainComponent.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="container">
     <div class="row mt-5 mb-5">
-    <div v-bind:class="[ isResultsOpen ? 'col-sm-7' : 'col-sm-12']">
+    <div v-bind:class="[ isErrorsOpen && !loading ? 'col-sm-7' : 'col-sm-12']">
       <form v-on:submit.prevent="auditContract()">
         <div class="d-flex justify-content-between">
           <div class="mb-3">
-            <button :disabled="(auditType === 'contract' && newAudit.contract.trim().length < 25) || (auditType === 'repository' && !repoUrl)" type="submit" class="btn btn-md btn-dark font-weight-bold montserrat px-5">Submit</button>
-            <button v-on:click="toggleAuditType()" v-if="auditType === 'contract'" type="button" class="btn btn-md btn-outline-dark font-weight-bold montserrat px-5"><i class="fab fa-github"></i> GitHub scan</button>
-            <button v-on:click="toggleAuditType()" v-if="auditType === 'repository'" type="button" class="btn btn-md btn-outline-dark font-weight-bold montserrat px-5"><i class="fas fa-code"></i> Code scan</button>
+            <button :disabled="loading || (auditType === 'contract' && newAudit.contract.trim().length < 25) || (auditType === 'repository' && !repoUrl)" type="submit" class="btn btn-md btn-dark font-weight-bold montserrat px-5">Submit</button>
+            <button :disabled="loading" v-on:click="toggleAuditType()" v-if="auditType === 'contract'" type="button" class="btn btn-md btn-outline-dark font-weight-bold montserrat px-5"><i class="fab fa-github"></i> GitHub scan</button>
+            <button :disabled="loading" v-on:click="toggleAuditType()" v-if="auditType === 'repository'" type="button" class="btn btn-md btn-outline-dark font-weight-bold montserrat px-5"><i class="fas fa-code"></i> Code scan</button>
           </div>
-          <button v-on:click="toggleResults()" v-if="!isResultsOpen" type="button" class="btn-md btn-outline-dark montserrat mb-3">Open Results tab</button>
         </div>
-        <editor v-if="auditType === 'contract'" v-model="newAudit.contract" @init="editorInit" lang="solidity" theme="mono_industrial" height="500"></editor>
-        <div v-if="auditType === 'repository'" class="row justify-content-center align-items-center text-center" style="height: 500px;">
+        <editor v-if="auditType === 'contract' && !loading" v-model="newAudit.contract" @init="editorInit" lang="solidity" theme="mono_industrial" height="500"></editor>
+        <div v-if="auditType === 'repository' && !loading" class="row justify-content-center align-items-center text-center" style="height: 500px;">
           <div class="col-sm-8">
             <h1><i class="fab fa-github"></i></h1>
             <div class="form-group">
@@ -23,29 +22,15 @@
           </div>
         </div>
       </form>
+      <div v-if="loading === true" style="height: 500px" class="d-flex justify-content-center align-items-center"><div class="loader"></div></div>
     </div>
-    <div class="col-sm-5" v-if="isResultsOpen">
-      <div class="results-labels">
-        <h4>Results</h4>
-        <small v-on:click="clearResults()"><a href="#">Clear</a></small>
-        <small v-on:click="toggleResults()"><a href="#">Close</a></small>
+    <div class="col-sm-5" v-if="isErrorsOpen">
+      <div class="errors-labels">
+        <h4>Errors</h4>
+        <small v-on:click="toggleErrors()"><a href="#">Close</a></small>
       </div>
-      <div class="alert alert-dark" role="alert" v-if="loading === false && issues.length === 0 && error === null">
-        Nothing to show.
-      </div>
-      <div class="loader" v-if="loading === true"></div>
 
       <div>
-        <div v-bind:class="[classMap[issue.severity]]" class="alert" role="alert" v-for="(issue, index) in issues" :key="index">
-          <p><b>{{ issue.description }}</b></p>
-          <p v-if="issue.severity !== null">Severity: <b>{{ severityMap[issue.severity] }}</b></p>
-          <p v-if="issue.contract">Contract: <b>{{ issue.contract }}</b></p>
-          <p v-if="issue.sourceMapping.lines">Line(s): {{ issue.sourceMapping.lines[0] }}<span v-if="issue.sourceMapping.lines.length > 1 && issue.sourceMapping.lines[0] != issue.sourceMapping.lines[issue.sourceMapping.lines.length - 1]">:{{ issue.sourceMapping.lines[issue.sourceMapping.lines.length - 1] }}</span></p>
-          <p v-if="issue.lines">Line(s): {{ issue.lines[0] }}<span v-if="issue.lines.length > 1 && issue.lines[0] != issue.lines[issue.lines.length - 1]">:{{ issue.lines[issue.lines.length - 1] }}</span></p>
-          <br/>
-          <p><b class="code" v-if="issue.info">{{ issue.info.replace(/\t/g, "") }}</b></p>
-        </div>
-
         <div class="alert alert-danger" role="alert" v-if="error !== null">
           <p class="text-center"><b>==== Compilation Error ====</b></p>
           <p>Error: {{ error.details }}</p>
@@ -67,11 +52,9 @@ export default {
   name: 'MainComponent',
   data () {
     return {
-      isResultsOpen: false,
+      isErrorsOpen: false,
       auditType: 'contract',
       loading: false,
-      classMap: ['alert-danger', 'alert-warning', 'alert-info', 'alert-dark'],
-      severityMap: ['High', 'Medium', 'Low', 'Informational'],
       issues: [],
       error: null,
       repoUrl: null,
@@ -107,7 +90,6 @@ contract SampleReentrancy {
   },
   methods: {
     auditContract () {
-      this.isResultsOpen = true
       this.loading = true
       this.issues = []
       this.error = null
@@ -124,6 +106,7 @@ contract SampleReentrancy {
           .catch((err) => {
             this.loading = false
             if (err.response.data && err.response.data.error) {
+              this.isErrorsOpen = true
               this.error = err.response.data
             }
 
@@ -135,19 +118,16 @@ contract SampleReentrancy {
             this.loading = false
             if (response.data && response.data.success) {
               this.$router.push({ path: `/github-audit/${response.data.tracking}` })
-              console.log(response.data, 'RESPONSE')
             }
           })
           .catch((err) => {
             this.loading = false
             if (err.response.data && err.response.data.error) {
+              this.isErrorsOpen = true
               this.error = err.response.data
             }
           })
       }
-    },
-    clearResults () {
-      this.issues = []
     },
     highlightError () {
       let codeElemNode = document.querySelectorAll('.ace_text-layer')
@@ -157,24 +137,17 @@ contract SampleReentrancy {
           line.style.background = lineNumber === this.error.lineno - 1 ? '#753131' : ''
         })
       }
-
-      let gutterElemNode = document.querySelectorAll('.ace_gutter-layer')
-      if (gutterElemNode && gutterElemNode.length > 0 && this.error) {
-        let gutterElem = gutterElemNode[0].childNodes
-        gutterElem.forEach((line, lineNumber) => {
-          line.style.background = lineNumber === this.error.lineno - 1 ? '#753131' : ''
-        })
-      }
     },
     editorInit () {
       require('brace/ext/language_tools')
       require('brace/theme/mono_industrial')
       require('../assets/js/solidity.js')
     },
-    toggleResults () {
-      this.isResultsOpen = !this.isResultsOpen
+    toggleErrors () {
+      this.isErrorsOpen = !this.isErrorsOpen
     },
     toggleAuditType () {
+      this.isErrorsOpen = false
       this.auditType = this.auditType === 'contract' ? 'repository' : 'contract'
     }
   },
