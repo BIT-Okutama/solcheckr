f5e33e88640b4708ff9d4fc7a411a8cd2bd5b1bd
Sam Benemerito <benemeritosam@gmail.com>
2018-12-10 16:44:45 +0900

    separate header to component

---
+++ frontend/src/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="header">
+  <nav class="navbar navbar-light bg-light">
+    <a href="/">SolCheckr</a>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Header from './Header'
 import router from './router'
 import './assets/css/app.scss'
 
@@ -9,6 +10,12 @@ Vue.config.productionTip = false
 Vue.set(Vue.prototype, '_', require('lodash'))
 
 /* eslint-disable no-new */
+new Vue({
+  el: '#header',
+  components: { Header },
+  template: '<Header/>'
+})
+
 new Vue({
   el: '#app',
   router,
