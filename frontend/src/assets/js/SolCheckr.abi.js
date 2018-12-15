fb38ddf3db3fe9782e781bb8b7990263ed1b4fbe
Sam Benemerito <benemeritosam@gmail.com>
2018-12-16 02:31:05 +0900

    add web3 requirements

---
+++ frontend/src/assets/js/SolCheckr.abi.js
@@ -0,0 +1,185 @@
+/* eslint-disable */
+const abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_auditType",
+                "type": "uint8"
+            }
+        ],
+        "name": "addAudit",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "author",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "auditType",
+                "type": "uint8"
+            }
+        ],
+        "name": "AuditAdded",
+        "type": "event"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_auditTracker",
+                "type": "bytes32"
+            },
+            {
+                "name": "_result",
+                "type": "bool"
+            }
+        ],
+        "name": "setAuditResult",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "name": "auditResults",
+        "outputs": [
+            {
+                "name": "isSet",
+                "type": "bool"
+            },
+            {
+                "name": "isSafe",
+                "type": "bool"
+            },
+            {
+                "name": "auditType",
+                "type": "uint8"
+            },
+            {
+                "name": "author",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_auditTracker",
+                "type": "bytes32"
+            }
+        ],
+        "name": "viewResult",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            },
+            {
+                "name": "",
+                "type": "uint8"
+            },
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+export default abi;
