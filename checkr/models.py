56e25f8799890ba2379d58d2bce3e9bffe37eb1f
Sam Benemerito <benemeritosam@gmail.com>
2018-12-16 13:51:59 +0900

    fix migrations (cant modify because of mixin)

--- checkr/models.py
+++ checkr/models.py
@@ -1,36 +1,25 @@
 from django.db import models
-from django.utils.crypto import get_random_string
-
-
-class TrackingMixin(object):
-    def save(self, *args, **kwargs):
-        if not self.tracking:
-            rand_str = get_random_string()
-            while self.__class__.objects.filter(tracking=rand_str).exists():
-                rand_str = get_random_string()
-
-            self.tracking = rand_str.strip()
-        super().save(*args, **kwargs)
 
 
 class BaseAuditModel(models.Model):
     report = models.TextField(blank=True, default='')
     result = models.NullBooleanField()
     submitted = models.DateTimeField(auto_now=True)
-    tracking = models.CharField(max_length=100, null=True, blank=True)
+    tracking = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
 
     class Meta:
         abstract = True
 
 
-class Audit(TrackingMixin, BaseAuditModel):
+class Audit(BaseAuditModel):
     contract = models.TextField()
 
 
-class GithubAudit(TrackingMixin, BaseAuditModel):
+class GithubAudit(BaseAuditModel):
     repo = models.CharField(max_length=200)
     contracts = models.TextField(blank=True, default='')
 
 
-class ZipAudit(TrackingMixin, BaseAuditModel):
+class ZipAudit(BaseAuditModel):
     contracts = models.TextField(blank=True, default='')
